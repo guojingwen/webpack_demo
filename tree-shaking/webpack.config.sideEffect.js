098339c666b91e5db0c0f295b2b4508147e517c5
guojw <2315162186@qq.com>
2020-05-24 17:08:32 +0800

    add

--- tree-shaking/webpack.config.sideEffect.js
+++ tree-shaking/webpack.config.sideEffect.js
@@ -1,12 +1,7 @@
 module.exports = {
   mode: 'none',
   optimization: {
-    // 模块只导出被使用的成员
-    usedExports: true,
-    // 尽可能合并每一个模块到一个函数中
-    // concatenateModules: true,
-    // 压缩输出结果
-    // minimize: false
+    sideEffects: true
   },
   entry: './src/main.js',
   output: {
